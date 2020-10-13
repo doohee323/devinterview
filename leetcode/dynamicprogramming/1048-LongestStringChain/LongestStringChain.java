3b8c517222fd18e2ca785dc0025eafbadc9d8c46
dhong <dhong@fortinet.com>
2020-10-13 11:59:59 -0700

    draft 1048	Longest String Chain 375	Guess Number Higher or Lower II

---
+++ leetcode/dynamicprogramming/1048-LongestStringChain/LongestStringChain.java
@@ -0,0 +1,22 @@
+package practices.dp.sentenceScreenFitting;
+
+public class LongestStringChain {
+
+	public int longestStrChain(String[] words) {
+
+    Map<String, Integer> map = new HashMap<>();
+    Arrays.sort(words, (a,b)->(a.length()-b.length()));
+
+    int ans = 0;
+    for(String w : words) {
+      int max = 0;
+      for(int i=0;i<w.length();i++) {
+        String tmp = w.substring(0, i) + w.substring(i + 1);
+        max = Math.max(max, map.getOrDefault(tmp, 0) + 1);
+      }
+      map.put(w, max);
+      ans = Math.max(ans, max);
+    }
+    return ans;
+  }
+}
---
+++ leetcode/dynamicprogramming/375-GuessNumberHigherOrLowerII/GuessNumberHigherOrLowerII.java
@@ -0,0 +1,50 @@
+package practices.dp.sentenceScreenFitting;
+
+public class GuessNumberHigherOrLowerII {
+
+// 1) MinMax Algo : https://www.youtube.com/watch?v=ZPtqO-d8eXg
+//   public int getMoneyAmount(int n) {
+//     return helper(0, n);
+//   }
+//   int helper(int l, int r) {
+//     if (l >= r) return 0;
+//     int ans = Integer.MAX_VALUE;
+//     for(int i=l;i<=r;i++) {
+//       ans = Math.min(ans, i + Math.max(helper(l, i-1), helper(i+1, r)));
+//     }
+//     return ans;
+//   }
+
+    // // 2) DP
+    // public int getMoneyAmount(int n) {
+    //   int[][] dp = new int[n + 1][n + 1];
+    //   return miniMax(1, n, dp);
+    // }
+    // int helper(int l, int r, int[][] dp){
+    //   if (l >= r) return 0;
+    //   if (dp[l][r] != 0)
+    //     return dp[l][r];
+    //   dp[l][r] = Integer.MAX_VALUE;
+    //   for(int i=l; i <= r; i++)
+    //     dp[l][r] = Math.min(dp[l][r], i + Math.max(helper(l, i-1, dp), helper(i+1, r, dp)));
+    //   return dp[l][r];
+    // }
+
+  // // 3) Optimize
+  public int getMoneyAmount(int n) {
+    int[][] dp = new int[n + 1][n + 1];
+    return helper(1, n, dp);
+  }
+  int helper(int l, int r, int[][] dp) {
+    if (l >= r) return 0;
+    if (dp[l][r] != 0)
+      return dp[l][r];
+    int ans = Integer.MAX_VALUE;
+    int m = l + (r - l) / 2; // mid point
+    for (int i=m; i <= r; i++)
+      ans = Math.min(ans, i + Math.max(helper(l, i-1, dp), helper(i+1, r, dp)));
+    dp[l][r] = ans;
+    return ans;
+  }
+
+}
